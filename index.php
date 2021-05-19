755b0b26f55a2c1ee70a9bdde9769840ea65963a
madinashb <74106100+madinashb@users.noreply.github.com>
2021-05-19 22:14:35 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+
+
+<html><head>
+  <link rel="stylesheet" href="style.css">
+    <title class="d">Madinas profile</title>
+    <style>
+        .leftpic {
+         float: left; 
+         margin: 1 1 6px 6px; 
+        }
+       </style>
+ </head><body style="background:grey; margin: 4px;">
+
+   <div style="background-color: white">
+   <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIeittjp16URuJ2pCevZ5Fg-AI_vRuL1NbMw&amp;usqp=CAU" width="200" height="272" class="leftpic">
+   	
+   	<h4>First name: Madina</h4> 
+   	<h4>Last name: Shakeeva</h4> 
+
+ 
+	<h4>Status: Student</h4>
+   	<h4> Univercity: IAAU  </h4>
+   	<h4>Faculty: Software computer system engeneering automated system</h4>
+    
+<footer style="position: static;">
+   <div style="position: relative; color: black">
+     <ul><hr>
+     	<li><p>Contacts</p></li>
+     	<p>Whatsapp: +996(772369726)</p>
+       <p>E-mail: madinashakeeva@iaau.edu.kg</p></hr>
+
+     </ul>
+  
+ 	</div>
+ </footer>
+
+</body></html>
