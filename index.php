30cb00c90ba70b62b322dfcd3761dfd96ed3110e
madinashb <74106100+madinashb@users.noreply.github.com>
2021-05-19 22:52:48 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
 
+<!DOCTYPE html>
 
 
-</body></html>
+<html><head>
+  <link rel="stylesheet" href="style.css">
+    <title class="d">Madinas profile</title>
+    <style>
+        .leftpic {
+         float: left; 
+         margin: 1 1 6px 6px; 
         }
        </style>
  </head><body style="background:grey; margin: 4px;">
 
-   <div style="background-color: #456360 ">
-   <img src="https://www.nicepng.com/png/full/136-1366211_group-of-10-guys-login-user-icon-png.png" width="200" height="200" class="leftpic">
-   	
+   <div style="background-color: #456360  ">
+   <img src="https://www.nicepng.com/png/full/136-1366211_group-of-10-guys-login-user-icon-png.png" width="190" height="190" class="leftpic">
+   	<hr>
    	<h4>First name: Madina</h4> 
    	<h4>Last name: Shakeeva</h4> 
-
+</hr>
  
 	<h4>Status: Student</h4>
-   	<h4> Univercity: IAAU  </h4>
+   	<a href="http://alatoo.edu.kg/" target="_blank" style="color: black">  University: IAAU </a>
+   
    	<h4>Faculty: Software computer system engeneering automated system</h4>
+   
     
-<footer style="position: static;">
-   <div style="position: relative; color: black">
+
+   
      <ul><hr>
      	<li><p>Contacts</p></li>
-     	<p>Whatsapp: +996(772369726)</p>
+     	<p>Whatsapp: +996(772)369726</p>
        <p>E-mail: madinashakeeva@iaau.edu.kg</p></hr>
 
      </ul>
   
- 	</div>
- </footer>
+ 		
+
 
 </body></html>
